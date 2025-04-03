9f408077df84c0964895c7bb0c85f83ecc868278
Miau <165878715+bema96@users.noreply.github.com>
2025-04-03 11:10:58 +0100

    Homepage/issue#9
    Testing connection to browser with route, but some fetching from last pull not working.
    Sorry if i fuck it up on this merge

---
+++ frontend/src/components/SearchBar/searchEngine.jsx
@@ -0,0 +1,24 @@
+import { SearchBarStyled } from "./search.styled";
+
+const SearchBar = () => {
+    return (
+        <SearchBarStyled>
+             <input
+        type="text"
+        placeholder="Søg café, by eller adresse..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        style={{
+          padding: "10px",
+          width: "100%",
+          marginBottom: "15px",
+          fontSize: "16px",
+          border: "1px solid #ccc",
+          borderRadius: "4px",
+        }}
+      />
+        </SearchBarStyled>
+    );
+};
+
+export default SearchBar;
